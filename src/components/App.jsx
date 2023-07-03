6e5cad8eb265ac2134519069199b3d52d829b3d8
Yulia Solovenuk <121509951+YuliyaSolovenuk@users.noreply.github.com>
2023-07-03 08:48:49 +0300

    upd/jsx/json

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,30 @@
+import data from 'data/data.json';
+import user from 'data/user.json';
+import friends from 'data/friends.json';
+import transactions from 'data/transactions.json';
+
+import { Profile } from './socialProfile/Profile';
+import { Statistics } from './statisticsSection/StatisticsList';
+import { FriendList } from './friendList/FriendList';
+import { TransactionHistory } from './transactionHistory/TransactionHistory';
+
 export const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
+    <div>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+
+      <Statistics title="Upload stats" stats={data} />
+
+      <FriendList friends={friends} />;
+
+      <TransactionHistory items={transactions} />;
+
     </div>
   );
 };
